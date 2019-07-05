347b22a84e680da57b469aa484b9baf6361be581
AlonsoSan <51421015+AlonsoSan@users.noreply.github.com>
2019-07-05 09:51:10 -0500

    Actualizacioones Finales

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# Proyecto POO II 
-
-| Nombre           | Código    | 
-| :-------------:  | :-------: | 
-| Alejandra Torres | 201810701 |
-| Alonso Sanchez   | 201810381 | 
-| Katherine Rengifo|           | 
+# game
+Proyecto final de POO
--- game.cpp
+++ game.cpp
@@ -2,31 +2,14 @@
 #include <SFML/Graphics.hpp>
 #include "Menu.h"
 
-int main() {
-    Menu menu;
+
+int main(){
+    TipoEntero altura , ancho;
+    cout << "Ingresar altura de ventana: ";
+    cin >> altura;
+    cout << "Ingresar ancho de ventana: ";
+    cin >> ancho;
+    Menu menu(ancho,altura);
     menu.ejecutar();
     return 0;
 }
-
-//int main(){
-//
-//    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
-//    while (window.isOpen()){
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-//                cout << event.mouseButton.x << "  " << event.mouseButton.y << endl;
-//                sf::RectangleShape shape(sf::Vector2f(50,50));
-//                shape.setFillColor(sf::Color(24,65,3,255));
-//                shape.setPosition(event.mouseButton.x,event.mouseButton.y);
-//                window.clear();
-//                window.draw(shape);
-//            }
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//            window.display();
-//        }
-//    }
-//    return 0;
-//}
